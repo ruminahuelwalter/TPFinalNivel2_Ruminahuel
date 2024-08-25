d74f3f3b686028e9ac09499a6f2b6eace6c2250b
ruminahuelwalter <ruminahuelwalter@gmail.com>
2024-08-25 17:10:58 -0300

    inicio AccesoDatos

---
+++ negocio/AccesoDatos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace negocio
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+    }
+}
