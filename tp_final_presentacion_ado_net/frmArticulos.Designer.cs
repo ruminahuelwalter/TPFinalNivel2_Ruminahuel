dceed10d83420f97e8bc2580075a82c0137587a5
ruminahuelwalter <ruminahuelwalter@gmail.com>
2024-08-26 17:32:43 -0300

    carga del DataGridView, carga de frmArticulos

--- tp_final_presentacion_ado_net/frmArticulos.Designer.cs
+++ tp_final_presentacion_ado_net/frmArticulos.Designer.cs
@@ -28,19 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvArticulos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvArticulos
+            // 
+            this.dgvArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvArticulos.Location = new System.Drawing.Point(38, 44);
+            this.dgvArticulos.Name = "dgvArticulos";
+            this.dgvArticulos.Size = new System.Drawing.Size(485, 272);
+            this.dgvArticulos.TabIndex = 0;
+            this.dgvArticulos.SelectionChanged += new System.EventHandler(this.dgvArticulos_SelectionChanged);
             // 
             // frmArticulos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(753, 447);
+            this.Controls.Add(this.dgvArticulos);
             this.Name = "frmArticulos";
             this.Text = "Articulos";
+            this.Load += new System.EventHandler(this.frmArticulos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvArticulos;
     }
 }
--- tp_final_presentacion_ado_net/frmArticulos.cs
+++ tp_final_presentacion_ado_net/frmArticulos.cs
@@ -7,14 +7,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using dominio;
+using negocio;
 
 namespace tp_final_presentacion_ado_net
 {
     public partial class frmArticulos : Form
     {
+        private List<Articulo> listaArticulo;
         public frmArticulos()
         {
             InitializeComponent();
+        }
+
+        private void dgvArticulos_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvArticulos.CurrentRow != null)
+            {
+                Articulo seleccionado = (Articulo)dgvArticulos.CurrentRow.DataBoundItem;
+                //cargarImagen(seleccionado.UrlImagen);
+            }
+        }
+
+        private void Cargar()
+        {
+            ArticuloNegocio negocio = new ArticuloNegocio();
+            try
+            {
+                listaArticulo = negocio.Listar();
+                dgvArticulos.DataSource = listaArticulo;
+                OcultarColumnas();
+                //cargarImagen(listaPokemon[0].UrlImagen);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void OcultarColumnas()
+        {
+            dgvArticulos.Columns["Id"].Visible = false;
+            dgvArticulos.Columns["UrlImagen"].Visible = false;
+        }
+
+        private void frmArticulos_Load(object sender, EventArgs e)
+        {
+            Cargar();
+
         }
     }
 }
