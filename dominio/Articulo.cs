cb1d14a8b153467f6717b83476c54236100fb1bc
ruminahuelwalter <ruminahuelwalter@gmail.com>
2024-08-26 16:32:58 -0300

    lista de articulos ArticuloNegocio

--- dominio/Articulo.cs
+++ dominio/Articulo.cs
@@ -19,6 +19,8 @@ namespace dominio
         public Categoria Categoria { get; set; }
         public Marca Marca { get; set; }
 
+        public double Precio { get; set; }
+
 
     }
 }
--- negocio/ArticuloNegocio.cs
+++ negocio/ArticuloNegocio.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using dominio;
 namespace negocio
 {
     public class ArticuloNegocio
     {
+        public List<Articulo> listar()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("Parametros de la consulta");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Codigo = (string)datos.Lector["Codigo"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+
+                    aux.Marca = new Marca();
+                    aux.Marca.Id = (int)datos.Lector["Id"];
+
+                    aux.Categoria = new Categoria();
+                    aux.Categoria.Id = (int)datos.Lector["IdMarca"];
+
+                    if (!(datos.Lector["UrlImagen"] is DBNull))
+                    {
+                        aux.UrlImagen = (string)datos.Lector["UrlImagen"];
+                    }
+
+                    aux.Precio = (double)datos.Lector["Precio"];
+
+
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+        }
     }
 }
